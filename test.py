b57b2537c2055bea089c75917415ba06494ff234
rijallatipudin <129058215+rijallatipudin@users.noreply.github.com>
2023-03-30 19:58:28 +0700

    Update test.py

--- test.py
+++ test.py
@@ -180,3 +180,138 @@ if st.button("Konversi"):
         st.success(f"{length} {unit1} = {result} {unit2}")
         st.text('satuan metrik (SI) : mm, cm, dm, m, dam, hm, km.')
         st.text('satuan imperial : inchi, kaki, mil, yard.')
+#halaman Konversi satuan luas
+if (selected == 'Konversi satuan luas') :
+    st.title('Konversi satuan luas')
+    length = st.number_input("Masukkan nilai yang akan dikonversi", min_value=0.0)
+    unit1 = st.selectbox("Pilih satuan awal", ["mm^2", "cm^2", "dm^2", "m^2", "dam^2", "hm^2", "km^2"])
+    unit2 = st.selectbox("Pilih satuan tujuan", ["mm^2", "cm^2", "dm^2", "m^2", "dam^2", "hm^2", "km^2"])
+
+    def convert(length, unit1, unit2):
+        if unit1 == "mm^2":
+            if unit2 == "cm^2":
+                return length / 100
+            elif unit2 == "dm^2":
+                return length / 10000
+            elif unit2 == "m^2":
+                return length / 1000000
+            elif unit2 == "dam^2":
+                return length / 100000000
+            elif unit2 == "hm^2":
+                return length / 10000000000
+            elif unit2 == "km^2":
+                return length / 1000000000000
+            elif unit2 == "mm^2":
+                return length / 1
+        
+        if unit1 == "cm^2":
+            if unit2 == "cm^2":
+                return length / 1
+            elif unit2 == "dm^2":
+                return length / 100
+            elif unit2 == "m^2":
+                return length / 10000
+            elif unit2 == "dam^2":
+                return length / 1000000
+            elif unit2 == "hm^2":
+                return length / 100000000
+            elif unit2 == "km^2":
+                return length / 10000000000
+            
+        if unit1 == "dm^2":
+            if unit2 == "dm^2":
+                return length / 1
+            elif unit2 == "m^2":
+                return length / 100
+            elif unit2 == "dam^2":
+                return length / 10000
+            elif unit2 == "hm^2":
+                return length / 1000000
+            elif unit2 == "km^2":
+                return length / 100000000
+            
+        if unit1 == "m^2":
+            if unit2 == "m^2":
+                return length / 1
+            elif unit2 == "dam^2":
+                return length / 100
+            elif unit2 == "hm^2":
+                return length / 10000
+            elif unit2 == "km^2":
+                return length / 1000000
+            
+        if unit1 == "dam^2":
+            if unit2 == "dam^2":
+                return length / 1
+            elif unit2 == "hm^2":
+                return length / 100
+            elif unit2 == "km^2":
+                return length / 10000
+            
+        if unit1 == "hm^2":
+            if unit2 == "hm^2":
+                return length / 1
+            elif unit2 == "km^2":
+                return length / 100
+        
+        if unit1 == "km^2":
+            if unit2 == "km^2":
+                return length / 1
+            
+        if unit1 == "km^2":
+            if unit2 == "hm^2":
+                return length * 100
+            elif unit2 == "dam^2":
+                return length * 10000
+            elif unit2 == "m^2":
+                return length * 1000000
+            elif unit2 == "dm^2":
+                return length * 100000000
+            elif unit2 == "cm^2":
+                return length * 10000000000
+            elif unit2 == "mm^2":
+                return length * 1000000000000
+    
+        if unit1 == "hm^2":
+            if unit2 == "dam^2":
+                return length * 100
+            elif unit2 == "m^2":
+                return length * 10000
+            elif unit2 == "dm^2":
+                return length * 1000000
+            elif unit2 == "cm^2":
+                return length * 100000000
+            elif unit2 == "mm^2":
+                return length * 10000000000
+            
+        if unit1 == "dam^2":
+            if unit2 == "m^2":
+                return length * 100
+            elif unit2 == "dm^2":
+                return length * 10000
+            elif unit2 == "cm^2":
+                return length * 1000000
+            elif unit2 == "mm^2":
+                return length * 100000000
+            
+        if unit1 == "m^2":
+            if unit2 == "dm^2":
+                return length * 100
+            elif unit2 == "cm^2":
+                return length * 10000
+            elif unit2 == "mm^2":
+                return length * 1000000
+        
+        if unit1 == "dm^2":
+            if unit2 == "cm^2":
+                return length * 100
+            elif unit2 == "mm^2":
+                return length * 10000
+
+        if unit1 == "cm^2":
+            if unit2 == "mm^2":
+                return length * 100
+
+    if st.button("Konversi"):
+        result = convert(length, unit1, unit2)
+        st.success(f"{length} {unit1} = {result} {unit2}")
